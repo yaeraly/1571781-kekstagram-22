688a4c4c482dfbac9af79408b24d3123df9e3138
yaeraly <ya.eraly@gmail.com>
2021-02-05 11:16:19 +0300

    Замечания исправлены

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const getRandomNumber = (min, max) => {
+
   if (min < 0) {
     return 'Начало диапазона должно быть положительным числом';
   }
-  else if (min >= max) {
+
+  if (min >= max) {
     return 'Начало диапазона должно быть меньше чем конец';
   }
 
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-getRandomNumber(0, 100);
+getRandomNumber(1.5, 99.5);
 
 
 const validateStringMaxLength = (string, maxLength = 140) => string.length <= maxLength;
