5ebd5cb99033d71177e4773043cf1dc07e215b5a
yaeraly <ya.eraly@gmail.com>
2021-03-27 16:12:21 +0300

    Перламутровые пуговицы (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,15 +1,14 @@
 import { isEscEvent, toggleModal } from './util.js';
 
-const fullPhoto         = document.querySelector('.big-picture');
-const closePhotoButton  = fullPhoto.querySelector('.big-picture__cancel');
-const commentsContainer = document.querySelector('.social__comments');
-const socialComment     = commentsContainer.querySelector('.social__comment');
-const fragment          = document.createDocumentFragment();
+const fullPhoto           = document.querySelector('.big-picture');
+const closePhotoButton    = fullPhoto.querySelector('.big-picture__cancel');
+const commentLoaderButton = fullPhoto.querySelector('.social__comments-loader');
+const commentsContainer   = document.querySelector('.social__comments');
+const socialComment       = commentsContainer.querySelector('.social__comment');
+const fragment            = document.createDocumentFragment();
 
 
 const renderComments = (arr) => {
-  commentsContainer.textContent = '';
-
   arr.forEach(({ avatar, message, name }) => {
     const comment = socialComment.cloneNode(true);
 
@@ -22,27 +21,47 @@ const renderComments = (arr) => {
   commentsContainer.appendChild(fragment);
 }
 
+
 const showFullPhoto = ( pictureElement, { url, description, likes, comments } ) => {
   pictureElement.addEventListener('click', (evt) => {
     evt.preventDefault();
-
-    toggleModal(fullPhoto);
+    commentsContainer.textContent = null;
 
     const commentsLength = comments.length;
 
-    const maxComments = Math.min(commentsLength, 5)
+    let commentStart = 0;
+    let commentEnd = Math.min(commentsLength, 5);
+
+    toggleModal(fullPhoto);
+
+    if (commentsLength <= 5) {
+      fullPhoto.querySelector('.social__comments-loader').classList.add('hidden');
+    } else {
+      fullPhoto.querySelector('.social__comments-loader').classList.remove('hidden');
+
+      const loadMoreComments = () => {
+        commentStart += 5;
+        if(commentEnd + 5 <= commentsLength) {
+          commentEnd += 5;
+        } else {
+          commentEnd = commentsLength;
+          commentLoaderButton.removeEventListener('click', loadMoreComments);
+          fullPhoto.querySelector('.social__comments-loader').classList.add('hidden');
+        }
+        renderComments(comments.slice(commentStart, commentEnd));
+        fullPhoto.querySelector('.social__comment-count').innerHTML = `${commentEnd} из <span class="comments-count">${commentsLength}</span> комментариев`;
+      }
+
+      commentLoaderButton.addEventListener('click', loadMoreComments);
+    }
 
     fullPhoto.querySelector('img').src = url;
     fullPhoto.querySelector('img').alt = description;
     fullPhoto.querySelector('.likes-count').textContent = likes;
     fullPhoto.querySelector('.social__caption').textContent = description;
-    fullPhoto.querySelector('.social__comment-count').innerHTML = `${maxComments} из <span class="comments-count">${commentsLength}</span> комментариев`;
+    fullPhoto.querySelector('.social__comment-count').innerHTML = `${commentEnd} из <span class="comments-count">${commentsLength}</span> комментариев`;
 
-    renderComments(comments.slice(0, maxComments));
-
-    // fullPhoto.querySelector('.social__comment-count').classList.add('hidden');
-    fullPhoto.querySelector('.social__comments-loader').classList.add('hidden');
-
+    renderComments(comments.slice(commentStart, commentEnd));
     addEventListeners();
   });
 }
@@ -52,7 +71,7 @@ const closeFullPhoto = () => {
   deleteEventListeners();
 }
 
-const closeFullPhotoEsc = (evt) => {
+const closeFullPhotoByEscape = (evt) => {
   if(isEscEvent(evt) && !fullPhoto.classList.contains('hidden')) {
     toggleModal(fullPhoto);
     deleteEventListeners();
@@ -61,12 +80,12 @@ const closeFullPhotoEsc = (evt) => {
 
 const addEventListeners = () => {
   closePhotoButton.addEventListener('click', closeFullPhoto);
-  document.addEventListener('keydown', closeFullPhotoEsc);
+  document.addEventListener('keydown', closeFullPhotoByEscape);
 }
 
 const deleteEventListeners = () => {
   closePhotoButton.removeEventListener('click', closeFullPhoto);
-  document.removeEventListener('keydown', closeFullPhotoEsc);
+  document.removeEventListener('keydown', closeFullPhotoByEscape);
 }
 
 export { showFullPhoto }
